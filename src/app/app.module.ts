c71a9d6c81222e8c38fdb832e4ec0abbcfbcb458
pendualIIDx <tonlnwforever@gmail.com>
2017-01-31 02:00:59 +0700

    edit primeng table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,7 +11,13 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
 import {routing} from './app.routing';
 import { TemplateComponent } from './template/template.component';
 import { AgmCoreModule } from 'angular2-google-maps/core';
+
+// primeng
 import {DropdownModule} from 'primeng/primeng';
+import {DataTableModule,SharedModule} from 'primeng/primeng';
+import {CarService} from './editroute/car/carservice';
+import {InputTextModule,ButtonModule,DialogModule} from 'primeng/primeng';
+
 import { PhotongComponent } from './photong/photong.component';
 import { PhotongstopComponent } from './photongstop/photongstop.component';
 import { EditrouteComponent } from './editroute/editroute.component';
@@ -31,11 +38,16 @@ import { EditrouteComponent } from './editroute/editroute.component';
     HttpModule,
     routing,
     DropdownModule,
+    DataTableModule,
+    SharedModule,
+    InputTextModule,
+    ButtonModule,
+    DialogModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBTT9UOlmNjSStQdUc0GcDXa2cfZG4EdB4'
     })
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/editroute/car/carservice.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Car} from '../../editroute/car/car';
+
+@Injectable()
+export class CarService {
+
+    constructor(private http: Http) {}
+
+    getCarsMedium() {
+        return this.http.get('app/resources/cars-medium.json')
+                    .toPromise()
+                    .then(res => <Car[]> res.json().data)
+                    .then(data => { return data; });
+    }
+}
