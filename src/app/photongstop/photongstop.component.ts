212e387aecd4d0097910bfcc321505554af672d1
pendualIIDx <tonlnwforever@gmail.com>
2017-01-31 23:26:56 +0700

    edit busstop component

--- src/app/photongstop/photongstop.component.ts
+++ src/app/photongstop/photongstop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-photongstop',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photongstop.component.css']
 })
 export class PhotongstopComponent implements OnInit {
+
+  val1: number;
+  val2: number;
 
   title: string = 'ป้ายจอดรถประจำทาง โพถ้อง ภูเก็ต';
   lat: number = 7.883135;
@@ -17,9 +21,26 @@ export class PhotongstopComponent implements OnInit {
   latB: number = 7.904144;
   lngB: number = 98.367557;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    // this.addMarker();
+  }
+
+  latlng() {
+    console.log(this.val1);
+    console.log(this.val2);
+    console.log(this.lat);
+    console.log(this.lng);
+    this.val1 = this.lat;
+    this.val2 = this.lng;
+    this.addMarker();
+  }
+
+  addMarker() {
+    this.lat = this.val1;
+    this.lng = this.val2;
+    this.router.navigate(['/photongstop']);
   }
 
 }
